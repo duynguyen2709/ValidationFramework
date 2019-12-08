4d5285da493ee4d9b201a68f6ea9af7ff97826c6
tim118 <tim@vinova.com.sg>
2019-12-08 10:55:43 +0700

    *add rule

---
+++ Rule/CompoundRule/IsPhoneNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Validation_Framework.rule
+{
+    class IsPhoneNumber :CompoundRule
+    {
+        public IsPhoneNumber() : this("Số điện thoại không hợp lệ")
+        {
+            AddRule(new MinLength(10));
+            AddRule(new MaxLength(10));
+            AddRule(new StartWith("0"));
+            AddRule(new IsNumber());
+        }
+
+        public IsPhoneNumber(string errorMessage) : base(errorMessage)
+        {
+            AddRule(new MinLength(10));
+            AddRule(new MaxLength(10));
+            AddRule(new StartWith("0"));
+            AddRule(new IsNumber());
+        }
+    }
+}
---
+++ Rule/SingleRule/HasLowerCase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Validation_Framework.rule
+{
+    class HasLowerCase : SingleRule
+    {
+
+        public HasLowerCase() : base("Chuỗi phải chưa ít nhất 1 kí tự thường")
+        {
+            value = 1;
+        }
+
+        public HasLowerCase(string message, int n = 1) : base(message)
+        {
+            value = n;
+        }
+        protected override bool CheckValid(dynamic target)
+        {
+            int count = (target as string).Count(c => char.IsLower(c));
+            if (count >= value) return true;
+            else return false;
+        }
+    }
+}
